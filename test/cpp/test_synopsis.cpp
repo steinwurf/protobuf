68fb5d1f9d15c480d64ece9bc2a5147f35619c39
techbech <pb@bechsoftware.com>
2023-10-05 11:05:02 +0200

    ..

---
+++ test/cpp/test_synopsis.cpp
@@ -0,0 +1,22 @@
+
+// Copyright (c) Steinwurf ApS 2020.
+// All Rights Reserved
+//
+// Distributed under the "BSD License". See the accompanying LICENSE.rst
+// file.
+
+#include <cstring>
+#include <gtest/gtest.h>
+
+#include "src.pb.h"
+#include <google/protobuf/util/json_util.h>
+
+TEST(test_synopsis, api)
+{
+    auto t = protobuf::Metrics();
+    t.set_protocol_version(1);
+
+    std::string json;
+    auto msg = google::protobuf::util::MessageToJsonString(t, &json);
+    EXPECT_TRUE(msg.ok());
+}
